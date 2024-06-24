75ee919d9173e5585b583720b5cf4dc7f37baf75
Lisbeth Cuenca Mamani <dioswachuka@gmail.com>
2024-06-23 20:47:08 -0400

    tercer parcial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
-import {HttpClientModule} from '@angular/common/http';
+
 import { MovieApiServiceService } from './service/movie-api-service.service';
+
 
 import {ReactiveFormsModule} from '@angular/forms';
 
@@ -18,13 +19,8 @@ import {ReactiveFormsModule} from '@angular/forms';
     SearchComponent,
     MovieDetailsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule
-  ],
-  providers: [MovieApiServiceService],
+
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
